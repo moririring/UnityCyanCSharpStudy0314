aac18f686298f3b909bf4209438bbce092a1f532
moririring <moriri.ring10@gmail.com>
2015-03-13 23:19:35 +0900

    追加

---
+++ Assets/CoinCreater.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CoinCreater : MonoBehaviour {
+
+	public GameObject CoinGameObject = new GameObject();
+    public float Speed = 7.0f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetButtonDown("Fire1"))
+		{
+            var offset = new Vector3(Mathf.Sin(Time.time) * Speed, 0, 0);
+            Instantiate(CoinGameObject, transform.position + offset, transform.rotation);
+		}
+	}
+}
---
+++ Assets/Pusher.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pusher : MonoBehaviour
+{
+
+    private Vector3 _origin;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    _origin = GetComponent<Rigidbody>().position;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var offset = new Vector3(0, 0, Mathf.Sin(Time.time));
+        this.GetComponent<Rigidbody>().MovePosition(_origin + offset);
+	
+	}
+}
